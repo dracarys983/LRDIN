cc2b4ae26de0bb87d859c4e46324d7a7848e8d1b
Kalpit Thakkar <ceasy983@gmail.com>
2017-04-10 15:56:42 +0530

    Some changes to main. DINet should be set up now

--- dinet.py
+++ dinet.py
@@ -1,4 +1,27 @@
 import torch.nn as nn
 
-def init(net, options):
-    
+class DINet(nn.Module):
+
+    def __init__(self, orig_model, arch, num_classes):
+        super(DINet, self).__init__()
+
+        if arch.startswith('alexnet'):
+            self.features = orig_model.features
+            self.classifier = nn.Sequential(nn.Dropout(),
+                                            nn.Linear(256 * 6 * 6, 4096),
+                                            nn.ReLU(inplace=True),
+                                            nn.Dropout(),
+                                            nn.Linear(4096, 4096),
+                                            nn.ReLU(inplace=True),
+                                            nn.Linear(4096, num_classes),
+                                            )
+            self.modelName = 'alexnet'
+        else:
+            raise NotImplementedError()
+
+
+    def forward(self, x):
+        f = self.features(x)
+        # y = self.classifier(f)
+
+        return f
--- main.py
+++ main.py
@@ -5,22 +5,29 @@ import torch.nn as nn
 import torch.utils.data
 from torchvision.models import models
 
-model_names = ('vgg16_bn', 'resnet50')
+import dinet
+import data
+
+model_names = ('alexnet', 'resnet50', 'vgg16')
 
 parser = argparse.ArgumentParser(description='PyTorch: UCF-101 Data Processing')
 parser.add_argument('--data', metavar='DIR', help='Path to Dataset')
-parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet50',
+parser.add_argument('--arch', '-a', metavar='ARCH', default='alexnet',
                     choices=model_names, help='model architecture: ' +
-                    ' | '.join(model_names) + ' (default: resnet50)')
+                    ' | '.join(model_names) + ' (default: alexnet)')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
+parser.add_argument('--classes', default=101, type=int,
+                    help='Number of output classes for Action Recognition')
 
 def main():
     global args, model
     args = parser.parse_args()
+    num_classes = args.classes
 
     print("=> using pre-trained model '{}'".format(args.arch))
-    model = models.__dict__[args.arcg](pretrained=True)
+    orig_model = models.__dict__[args.arch](pretrained=True)
+    dynamicImageNet = DINet(orig_model, args.arch, num_classes)
 
     if args.resume:
         if os.path.isfile(args.resume):
@@ -34,4 +41,4 @@ def main():
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    
+    cudnn.benchmark = True
