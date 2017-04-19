0225daf75497cca205e6f67feba8ac154cb31ce8
Kalpit Thakkar <ceasy983@gmail.com>
2017-04-19 19:29:23 +0530

    AppRankPool layer forward pass done

--- layers.py
+++ layers.py
@@ -10,20 +10,21 @@ class ApproximateRankPooling(nn.Module):
         self.batch_images = x
         self.nvids = nvids
         self.dframes = dframes
-        self.out_size_fwd = np.insert(np.array(X.size()[1:]), 0, nVids, axis=0).tolist()
+        self.out_size_fwd = np.insert(np.array(x.size()[1:]), 0, nvids, axis=0).tolist()
 
     def forward(self, x):
         out_dynamic_images = np.zeros(self.out_size_fwd)
+        x = x.numpy()
         for v in range(self.nvids):
-            idv = [x for x in range(v*self.dframes,(v+1)*self.dframes)]
+            idv = [i for i in range(v*self.dframes,(v+1)*self.dframes)]
             nmagic = np.zeros(self.dframes)
             if self.dframes == 1:
                 nmagic = 1
             else:
                 for i in range(self.dframes):
-                    nmagic[i] = sum([2*x-self.dframes-1/(x*1.0) for x in list(range(i,self.dframes+1))])
-            x = x.numpy()
-            out_dynamic_images[v] = sum([x[i]*(np.reshape(nmagic,(self.dframes,1,1,1))) for i in idv], 4)
+                    nmagic[i] = sum([2*k-self.dframes-1/(k*1.0) for k in list(range(i,self.dframes+1))])
+            x = x[idv, :, :, :]
+            out_dynamic_images[v] = np.sum([x*(np.reshape(nmagic,(self.dframes,1,1,1)))], axis=0)
         return torch.Tensor(out_dynamic_images)
 
     def backward(self):
