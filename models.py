41390fb775a1c87627c44f37d7f8c22a4969e06b
Kalpit Thakkar <ceasy983@gmail.com>
2017-05-01 19:26:11 +0530

    Complete the forward pass

--- models.py
+++ models.py
@@ -1,4 +1,9 @@
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+import numpy as np
 
 import layers
 
@@ -7,7 +12,8 @@ class DINet(nn.Module):
     def __init__(self, orig_model, arch, num_classes):
         super(DINet, self).__init__()
 
-        self.arpool = nn.Sequential(layers.ApproximateRankPooling())
+        self.arpool = layers.ApproximateRankPooling()
+        self.l2norm = layers.L2Normalize()
         if arch.startswith('alexnet'):
             self.features = orig_model.features
             self.classifier = nn.Sequential(nn.Dropout(),
@@ -24,6 +30,22 @@ class DINet(nn.Module):
 
     def forward(self, x, vidids):
         dyn = self.arpool(x, vidids)
-        f = self.features(dyn)
-        y = self.classifier(f)
-        return y
+        result = Variable(torch.FloatTensor(dyn.size(0), dyn.size(1), 1, 101))
+        inter = Variable(torch.FloatTensor(dyn.size(1), 1, 101))
+        b = 0
+        for batch in dyn:
+            i = 0
+            # Debug code: Save and check dynamic images formed
+            # fname = 'dynamic_image_' + str(b) + '.jpg'
+            # save_image(tensor=batch.data, filename=fname)
+            for img in batch:
+                # Send in dimensions 1 x C x H x W (for a single image)
+                img = img.view(-1, img.size(0), img.size(1), img.size(2))
+                f = self.features(img)
+                f = f.view(f.size(0), 256 * 6 * 6)
+                c = self.classifier(f)
+                inter[i, :, :] = c
+                i += 1
+            result[b, :, :, :] = inter
+            b += 1
+        return result
