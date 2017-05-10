dcbfffa930da45c2071c58a05c93d074cd951a22
Kalpit Thakkar <ceasy983@gmail.com>
2017-05-10 16:57:41 +0530

    Change the path to data/ folder (relative)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python main.py --outdir '/Users/dracarys983/data' --traintest './ucfTrainTestlist/temptrainlist04.txt' --classfile './ucfTrainTestlist/classInd.txt'
+python main.py --outdir '../data' --traintest './ucfTrainTestlist/temptrainlist04.txt' --classfile './ucfTrainTestlist/classInd.txt'
