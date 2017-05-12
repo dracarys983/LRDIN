e4dfef84a6f2e0f98bac09fe63acbdaa48065331
dracarys983 <ceasy983@gmail.com>
2017-05-12 14:57:53 +0530

    Add some comments and layers in batch loop

--- models.py
+++ models.py
@@ -12,11 +12,11 @@ class DINet(nn.Module):
     def __init__(self, orig_model, arch, num_classes):
         super(DINet, self).__init__()
 
-        self.arpool = layers.ApproximateRankPooling()
-        self.l2norm = layers.L2Normalize()
         if arch.startswith('alexnet'):
+            self.arpool = layers.ARPool()
+            self.l2norm = layers.L2Norm()
             self.features = orig_model.features
-            self.temppool = layers.TemporalPooling()
+            self.temppool = layers.TempPool()
             self.classifier = nn.Sequential(nn.Dropout(),
                                             nn.Linear(256 * 6 * 6, 4096),
                                             nn.ReLU(inplace=True),
@@ -33,21 +33,24 @@ class DINet(nn.Module):
     def forward(self, x, vidids):
         params = [6e3, -128, 128, 0]
         params = Variable(torch.from_numpy(np.array(params)))
-        # Initialize the result and intermediate tensors
-        result = Variable(torch.FloatTensor(nimgs.size(0), 101))
+        # Initialize the result tensor
+        result = Variable(torch.FloatTensor(x.size(0), 101))
         b = 0
         # Forward pass through Alexnet
         for batch in x:
             # Approxmiate Rank Pooling layer : Get Dynamic Images
-            dyn = self.arpool(batch, vidids)
+            dyn = self.arpool(batch, vidids[b])
             # L2Normalize layer : Normalize the Dynamic Images
             nimgs = self.l2norm(dyn, params)
             # Debug code: Save and check dynamic images formed
             # fname = 'dynamic_image_' + str(b) + '.jpg'
             # save_image(tensor=nimgs.data, filename=fname)
+            # Convolutional layers (get the features)
             f = self.features(nimgs)
+            # TemporalPooling layer : Pool across Dynamic Images
             t = self.temppool(f)
             t = t.view(t.size(0), 256 * 6 * 6)
+            # Classification layers (Fully Connected) with Softmax
             c = self.classifier(t)
             result[b, :] = c[0]
             b += 1
